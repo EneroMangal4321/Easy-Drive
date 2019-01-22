9caf6ebb9c53b6f70a9ecba1a5d05786c8fe5558
EneroMangal4321 <45204052+EneroMangal4321@users.noreply.github.com>
2019-01-22 20:24:52 +0100

    ik heb mn best gedaan man..

--- Javascript.js
+++ Javascript.js
@@ -1,5 +1,4 @@
-//Gemaakt door Enero & Ricardo
-var auto10 = 350;
+/*var auto10 = 350;
 var auto20 = 700;   
 var auto50 = 1750;  
 var autoLos =  50;
@@ -16,36 +15,19 @@ var Motor20 =  500;
 var Motor50 =  1250;
 var MotorLos = 40;
 var MotorTheorie_Praktijk = 100;
-var MotorTheorie = 70;
+var MotorTheorie = 70;*/
 
-
-document.getElementById('auto10').value;
-document.getElementById('auto20').value;
-document.getElementById('auto50').value;
-document.getElementById('autoLos').value;
-document.getElementById('autoTheorie_Praktijk').value;
-document.getElementById('autoTheorie').value;
-document.getElementById('Vracht10').value;
-document.getElementById('Vracht20').value;
-document.getElementById('Vracht50').value;
-document.getElementById('VrachtLos').value;
-document.getElementById('VrachtTheorie_Praktijk').value;
-document.getElementById('VrachtTheorie').value;
-document.getElementById('Motor10').value;
-document.getElementById('Motor20').value;
-document.getElementById('Motor50').value;
-document.getElementById('MotorLos').value;
-document.getElementById('MotorTheorie_Praktijk').value;
-document.getElementById('MotorTheorie').value;
-
-
-//Einde alle pakketten
-
-//pakketten veranderen naar floats
-
-//Einde van floats maken
-
-//Berekenen hoeveel er in je winkelwagentje zit
-function bereken() {
-    
+function bereken(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
 }
+document.getElementsByClassName('test').innerHTML = bereken();
+
+
+
+
+
+
+
