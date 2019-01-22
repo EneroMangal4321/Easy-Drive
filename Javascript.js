f9abcfdd9817bf7742bb73a573e198c47ab4ce37
amanvir23 <45204255+amanvir23@users.noreply.github.com>
2019-01-22 22:20:26 +0100

    nieuwe shoppingcard

---
+++ Javascript.js
@@ -0,0 +1,145 @@
+
+
+function AUTO10(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO10();
+
+function AUTO20(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO20();
+
+function AUTO50(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO50();
+
+function AUTO_LOS(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO_LOS();
+
+function AUTO_TP(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO_TP();
+
+function AUTO_T(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = AUTO_T();
+
+function VRACHT10(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT10();
+
+function VRACHT20(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT20();
+
+function VRACHT50(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT50();
+
+function VRACHT_LOS(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT_LOS();
+
+function VRACHT_TP(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT_TP();
+
+function VRACHT_T(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = VRACHT_T();
+
+function MOTOR10(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR10();
+
+function MOTOR20(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR20();
+
+function MOTOR50(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR50();
+
+function MOTOR_LOS(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR_LOS();
+
+function MOTOR_TP(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR_TP();
+
+function MOTOR_T(x, y) {
+    if (y === undefined){
+        y = 1;
+    }
+    return x*y;
+}
+document.getElementsById("resultaat").innerHTML = MOTOR_T();
