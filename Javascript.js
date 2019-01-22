f3ce5e8dd05a8916b76a24ac358084f8db4c5db0
EneroMangal4321 <45204052+EneroMangal4321@users.noreply.github.com>
2019-01-22 19:15:11 +0100

    gaat m niet worden man

--- Javascript.js
+++ Javascript.js
@@ -3,40 +3,40 @@ var auto10 = 350;
 var auto20 = 700;   
 var auto50 = 1750;  
 var autoLos =  50;
-var autoTheorie_Praktijk = 
-var autoTheorie = 
-var Vracht10 = 
-var Vracht20 = 
-var Vracht50 = 
-var VrachtLos = 
-var VrachtTheorie_Praktijk = 
-var VrachtTheorie = 
-var Motor10 =  
-var Motor20 =  
-var Motor50 =  
-var MotorLos = 
-var MotorTheorie_Praktijk = 
-var MotorTheorie = 
+var autoTheorie_Praktijk = 120;
+var autoTheorie = 90;
+var Vracht10 = 450;
+var Vracht20 = 900;
+var Vracht50 = 2250;
+var VrachtLos = 60
+var VrachtTheorie_Praktijk = 210;
+var VrachtTheorie = 180;
+var Motor10 =  250;
+var Motor20 =  500;
+var Motor50 =  1250;
+var MotorLos = 40;
+var MotorTheorie_Praktijk = 100;
+var MotorTheorie = 70;
 
 
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
-document.getElementById('').value;
+document.getElementById('auto10').value;
+document.getElementById('auto20').value;
+document.getElementById('auto50').value;
+document.getElementById('autoLos').value;
+document.getElementById('autoTheorie_Praktijk').value;
+document.getElementById('autoTheorie').value;
+document.getElementById('Vracht10').value;
+document.getElementById('Vracht20').value;
+document.getElementById('Vracht50').value;
+document.getElementById('VrachtLos').value;
+document.getElementById('VrachtTheorie_Praktijk').value;
+document.getElementById('VrachtTheorie').value;
+document.getElementById('Motor10').value;
+document.getElementById('Motor20').value;
+document.getElementById('Motor50').value;
+document.getElementById('MotorLos').value;
+document.getElementById('MotorTheorie_Praktijk').value;
+document.getElementById('MotorTheorie').value;
 
 
 //Einde alle pakketten
