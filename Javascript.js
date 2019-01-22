e8fa2cdee49d2fe12e063de3abd22ef2cf14899a
RicardoROC <45204118+RicardoROC@users.noreply.github.com>
2019-01-22 13:37:52 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,16 +1,16 @@
 //Alle pakketten
 var auto10 = 
-var auto20
-var auto50
-var autolos1
-var theorieexamenpraktijk
-var theorieexamen
-var vrachtwagen10
-var vrachtwagen20
-var vrachtwagen50
-var vrachtwagenlos1
-var theorieexamenpraktijkvracht
-var theorieexamenvracht
+var auto20 = 
+var auto50 =
+var autolos1 = 
+var theorieexamenpraktijk = 
+var theorieexamen = 
+var vrachtwagen10 = 
+var vrachtwagen20 = 
+var vrachtwagen50 = 
+var vrachtwagenlos1 = 
+var theorieexamenpraktijkvracht = 
+var theorieexamenvracht =
 var motor10 
 var motor20
 var motor50
